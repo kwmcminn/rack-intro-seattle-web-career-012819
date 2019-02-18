5da0bfa66b29cfd53bd4c6a5091c37aebf942968
kwmcminn <kwmcminn@gmail.com>
2019-02-18 09:22:12 -0800

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,8 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is KEVIN! Today is Monday, February 18th. Today is Preseident's day."
     resp.finish
   end
 
 end
-
